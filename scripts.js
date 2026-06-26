ce106e933566c8e5e33d118d6a5d0763be1b54e4
funshirtsarenicetohave <jorgfischer0@gmail.com>
2026-06-26 14:12:10 +0200

    scripts.js

--- scripts.js
+++ scripts.js
@@ -9,4 +9,18 @@ const designs = [
   }
 ];
 
-console.log(designs);
+const grid = document.getElementById("designGrid");
+
+designs.forEach(design => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <img src="${design.image}" alt="${design.title}">
+    <h2>${design.title}</h2>
+    <p>${design.collection}</p>
+    <a class="button" href="${design.amazonDE}" target="_blank">🇩🇪 Auf Amazon.de ansehen</a>
+  `;
+
+  grid.appendChild(card);
+});
