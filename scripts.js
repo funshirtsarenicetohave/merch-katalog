d8c040795b53269eb4d9df29582022a5e1967a40
funshirtsarenicetohave <jorgfischer0@gmail.com>
2026-06-27 20:27:43 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -261,11 +261,11 @@ const visibleDesigns =
     const card = document.createElement("div");
     card.className = "card";
 
-card.innerHTML =
-  '<div class="image-box"><img src="' + design.image + '" alt="' + design.title + '"></div>' +
-  '<h2>' + design.title + '</h2>' +
-  '<p>' + design.collection + '</p>' +
-  '<a class="button" href="' + design.amazonDE + '" target="_blank">🇩🇪 Auf Amazon.de ansehen</a>';
+  card.innerHTML =
+    '<div class="image-box"><img src="' + design.image + '" alt="' + design.title + '"></div>' +
+    '<h2>' + design.title + '</h2>' +
+    '<p>' + design.collection + '</p>' +
+    '<a class="button" href="' + design.amazonDE + '" target="_blank">🇩🇪 Auf Amazon.de ansehen</a>';
   });
 
   section.appendChild(heading);
