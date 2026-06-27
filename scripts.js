5fa51665f4f5574f1567ea57f3440877cfd38a1c
funshirtsarenicetohave <jorgfischer0@gmail.com>
2026-06-27 20:31:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -266,6 +266,8 @@ const visibleDesigns =
     '<h2>' + design.title + '</h2>' +
     '<p>' + design.collection + '</p>' +
     '<a class="button" href="' + design.amazonDE + '" target="_blank">🇩🇪 Auf Amazon.de ansehen</a>';
+  
+    collectionGrid.appendChild(card);
   });
 
   section.appendChild(heading);
